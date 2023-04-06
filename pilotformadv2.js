b0107dc14e54fbc31f665f4db84c85906a8ac805
Ghulam Murtza <47827897+Ghulam-Murtza@users.noreply.github.com>
2023-04-06 10:32:14 -0500

    Create pilotformadv2.js

---
+++ pilotformadv2.js
@@ -0,0 +1,337 @@
+var Flight_Experience, Medical_Conditions, Criminal_History, Time, Become_pilot, Get_Started, Name, name, email, phone;
+
+
+var we_tabs_next_button = '[bloc=next-question]';
+    var we_tabs_prev_button = '[bloc=prev-question]';
+    var we_tabs_active_class = 'active';
+
+    var we_activeTab,we_indexOfActiveTab, we_indexOfNextTab, we_indexOfPrevTab, we_prevTab, we_amountOfTabs, nextTab;
+
+    var tabList = ['Flight_Experience'];
+
+    openTab(tabList[0]);
+
+    $(we_tabs_next_button).on("click", function () {
+      var activeTab = $(".w--tab-active").attr("data-w-tab");
+      var indexOfActiveTab = tabList.indexOf(activeTab);
+      var indexOfNextTab = indexOfActiveTab + 1;
+      var nextTab = tabList[indexOfNextTab];
+      var amountOfTabs = tabList.length;
+      var attrNextTab = $('.w--tab-active').attr('next-tab');
+
+      openTab(attrNextTab);
+
+      attrNextTab = $('.w--tab-active').attr('next-tab');
+
+      if (typeof attrNextTab !== 'undefined' && attrNextTab !== false) {
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+      } else {
+        $(we_tabs_next_button).removeClass(we_tabs_active_class);
+      }
+
+      if (amountOfTabs > 1) {
+        $(we_tabs_prev_button).addClass(we_tabs_active_class);
+      }
+    });
+
+    $(we_tabs_prev_button).on("click", function () {
+      var activeTab = $(".w--tab-active").attr("data-w-tab");
+      var indexOfActiveTab = tabList.indexOf(activeTab);
+      var indexOfPrevTab = indexOfActiveTab - 1;
+      var prevTab = tabList[indexOfPrevTab];
+
+      var attrPrevTab = $('.w--tab-active').attr('prev-tab');
+
+      if (indexOfActiveTab > 0) {
+        openTab(attrPrevTab);
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+      }
+
+      if (indexOfActiveTab <= 1) {
+        $(we_tabs_prev_button).removeClass(we_tabs_active_class);
+      } else {
+        $(we_tabs_prev_button).addClass(we_tabs_active_class);
+      }
+    });
+
+    function openTab(tab) {
+      var tab_link = "a[data-w-tab=" + tab;
+      tab = "div[data-w-tab=" + tab;
+
+      $(tab).siblings().removeClass("w--tab-active");
+      $(tab).addClass("w--tab-active");
+      $(tab_link).siblings("a").removeClass("w--current");
+      $(tab_link).addClass("w--current");
+    }$("[name=Flight_Experience]").on("input", function () {
+    Flight_Experience = getValueFromInput("Flight_Experience");
+
+      nextTab = 'Medical_Conditions';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Medical_Conditions]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Flight_Experience]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Flight_Experience = getValueFromInput("Flight_Experience");
+
+      nextTab = 'Medical_Conditions';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Medical_Conditions]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Medical_Conditions]").on("input", function () {
+    Medical_Conditions = getValueFromInput("Medical_Conditions");
+
+      nextTab = 'Criminal_History';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Criminal_History]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Medical_Conditions]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Medical_Conditions = getValueFromInput("Medical_Conditions");
+
+      nextTab = 'Criminal_History';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Criminal_History]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Criminal_History]").on("input", function () {
+    Criminal_History = getValueFromInput("Criminal_History");
+
+      nextTab = 'Time';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Time]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Criminal_History]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Criminal_History = getValueFromInput("Criminal_History");
+
+      nextTab = 'Time';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Time]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Time]").on("input", function () {
+    Time = getValueFromInput("Time");
+
+      nextTab = 'Become_pilot';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Become_pilot]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Time]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Time = getValueFromInput("Time");
+
+      nextTab = 'Become_pilot';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Become_pilot]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Become_pilot]").on("input", function () {
+    Become_pilot = getValueFromInput("Become_pilot");
+
+      nextTab = 'Get_Started';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Get_Started]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Become_pilot]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Become_pilot = getValueFromInput("Become_pilot");
+
+      nextTab = 'Get_Started';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Get_Started]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Get_Started]").on("input", function () {
+    Get_Started = getValueFromInput("Get_Started");
+
+      nextTab = 'Name';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Email]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=Get_Started]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Get_Started = getValueFromInput("Get_Started");
+
+      nextTab = 'Name';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=Name]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=Name]").on("input", function () {
+    Name = getValueFromInput("Name");
+    Email = getValueFromInput("Email");
+    Phone = getValueFromInput("Phone");
+      if (name != (null && email != null)) {
+    window.location.replace('https://www.av8prep.com/success');
+    window.location.href('https://www.av8prep.com/success');
+  }
+});
